35da1ee5f772d0be96c69c8843e9d22fdf38f072
RyanBurgert219 <sqrtryan@gmail.com>
2019-06-06 01:18:34 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import  two from 'twojs-ts'
+function Two(props)
+{
+	const [two ,setTwo ]=React.useState(null)
+	const [elem,setElem]=React.useState(null)//The dom element we're rendering to
+	if(elem&&!two)
+	{
+		let params={width: 29958, height: 200}
+		let newTwo=new window.Two(params)
+		setTwo(newTwo)
+		console.log(elem)
+		newTwo.appendTo(elem)
+	}
+	if(two)
+	{
+		let circle=two.makeCircle(72, 100, 50)
+		let rect  =two.makeRectangle(213, 100, 100, 100)
+
+		// The object returned has many stylable properties:
+		circle.fill     ='#FF8000'
+		circle.stroke   ='orangered' // Accepts all valid css color
+		circle.linewidth=props.width
+
+		rect.fill   ='rgb(0, 200, 255)'
+		rect.opacity=0.75
+		rect.noStroke()
+
+		// Don't forget to tell two to render everything
+		// to the screen
+		two.update()
+	}
+	return <div ref={setElem} {...props}/>
+
+}
 function App()
 {
-	return <div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo"/>
-			<p>
-				Edit <code>src/App.js</code> and save to reload.
-			</p>
-			<a
-				className="App-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn React
-			</a>
-		</header>
-	</div>
+	const [w,sw]=React.useState(0)
+	return <>
+		<Two width={w} onMouseMove={()=>sw(w+1)}/>
+		<Two width={w}/>
+	</>
 }
 
 export default App
