9bb10d1ca3911b7daccdc71d55a9bf6860b946a4
RyanBurgert219 <sqrtryan@gmail.com>
2019-06-06 01:28:08 -0400

    Working on a generic way of including any dom stuff into react as can be seen in App.js

--- src/App.js
+++ src/App.js
@@ -33,6 +33,38 @@ function Two(props)
 	return <div ref={setElem} {...props}/>
 
 }
+function Generalized(props)
+{
+	//TODO: To truly understand the above code, try generaliing it to any generic dom plugin like twojs or threejs
+	const [plug,setPlug]=React.useState(null)//Some plugin or something like two.js
+	const [elem,setElem]=React.useState(null)//The dom element we're rendering to
+	if(elem&&!plug)
+	{
+		let newPlug=props.newPlug()
+		setPlug(newPlug)
+		newPlug.appendTo(elem)
+	}
+	if(plug)
+	{
+		let circle=plug.makeCircle(72, 100, 50)
+		let rect  =plug.makeRectangle(213, 100, 100, 100)
+
+		// The object returned has many stylable properties:
+		circle.fill     ='#FF8000'
+		circle.stroke   ='orangered' // Accepts all valid css color
+		circle.linewidth=props.width
+
+		rect.fill   ='rgb(0, 200, 255)'
+		rect.opacity=0.75
+		rect.noStroke()
+
+		// Don't forget to tell plug to render everything
+		// to the screen
+		plug.update()
+	}
+	return <div ref={setElem} {...props}/>
+
+}
 function App()
 {
 	const [w,sw]=React.useState(0)
